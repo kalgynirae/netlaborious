e105dc2f8a7bbb9535039441bc14b6af00a8694e
Colin Chan <colinchan@lumeh.org>
2014-12-13 15:34:38 -0500

    WIP #2

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 **netlaborious** is a set of Python tools to ease managing vSphere and NETLAB+
 setups.
 
-# Usage
+# Setup
 
-Create a virtualenv, install dependencies, run the script.
+Create a virtualenv and install dependencies:
 
     $ virtualenv env
     $ env/bin/pip install -r requirements.txt
+
+Then run the script using the virtualenv Python:
+
     $ env/bin/python netlaborious.py
+
+# Usage
+
+    $ netlaborious [--verbose] [--dry-run] <command> [options]
+
+## Batch mode
+
+If `<command>` is `batch`, netlaborious will read lines of `<command> [options]`
+from stdin.  This is done using Python's `shlex` module, so normal shell-style
+word splitting is performed.  If a line begins with the special command `ARGS`,
+the arguments specified on that line will be applied to every following command
+(or until the next `ARGS` line).
--- netlaborious.py
+++ netlaborious.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 """
-usage: netlaborious [--verbose] [--dry-run]
-                    (batch|upload|clone|mkpod|rmpod) [options]
+usage: netlaborious [-v] [-d] (batch|upload|clone) [options]
        netlaborious --help
 
 Commands:
@@ -11,8 +10,6 @@ Commands:
                         a snapshot of it.
   clone                 Clone an existing VM to all other hosts (and make
                         snapshots).
-  mkpod                 Create a set of NETLAB+ pods and map them to VMs.
-  rmpod                 Delete a set of NETLAB+ pods.
 
 Options:
   -v, --verbose         print detailed messages for debugging
@@ -27,7 +24,6 @@ Options:
   --provisioning PROV   provisioning type [default: thin]
   --vm NAME             name of the VM to be created/attached/used
   --snapshot NAME       name of snapshot (if not specified, no snapshot)
-  --pod NAME            name of the pod to create/remove
 """
 from __future__ import print_function
 import contextlib
@@ -58,6 +54,9 @@ class DryRunDummy(object):
     def __getattr__(self, name):
         return self
 
+    def __getitem__(self, key):
+        return self
+
 
 class MissingArgumentsError(Exception):
     pass
@@ -78,28 +77,30 @@ def args(*option_names):
                 raise MissingArgumentsError('%s requires option(s) %s' %
                                             (func.__name__, ', '.join(missing)))
             else:
+                s = ', '.join('%s=%s' % (name, value)
+                              for name, value in zip(option_names, values))
+                logger.debug('calling %s(%s)' % (func.__name__, s))
                 return func(*values)
         return wrapped
     return args_decorator
 
-@args('vshost vsuser ovf vm dest-host dest-folder network provisioning'.split())
-def action_upload(host, user, ovf_path, vm_name, dest_hostname, dest_folder,
-                  network, provisioning):
-    logger.debug('Uploading OVF {} to host {} in folder {} with network {} and '
-                 'provisioning {}'
-                 .format(ovf, host, folder, network, provisioning))
-    with vsphere_connection(host, username, port) as conn:
+
+@args(*'vshost vsport vsuser ovf vm dest-host dest-folder network provisioning'
+       .split())
+def action_upload(host, port, user, ovf_path, vm_name, dest_hostname,
+                  dest_folder, network, provisioning):
+    with vsphere_connection(host, user, port) as conn:
         content = conn.RetrieveContent()
-        #with open(ovf) as ovf:
-        #    result = content.ovfManager.ParseDescriptor(ovf.read(), <pdp>)
-        datacenter = content.rootFolder.childEntity[0]
-        vmfolder = datacenter.vmFolder
-        hosts = datacenter.hostFolder.childEntity
-        resource_pool = hosts[0].resourcePool
-        print(locals())
+        params = pyVmomi.vim.OvfManager.ParseDescriptorParams()
+        with open(ovf) as f:
+            result = content.ovfManager.ParseDescriptor(f.read(), params)
+        #datacenter = content.rootFolder.childEntity[0]
+        #vmfolder = datacenter.vmFolder
+        #hosts = datacenter.hostFolder.childEntity
+        #resource_pool = hosts[0].resourcePool
 
 
-@args('vshost vsuser source-vm dest-host snapshot-name'.split())
+@args(*'vshost vsuser vm dest-host snapshot'.split())
 def action_clone(host, user, source_vm, dest_host, snapshot_name):
     with pysphere_connection(host, user) as server:
         logger.debug('Fetching VM {!r}'.format(source_vm))
@@ -127,9 +128,9 @@ def action_rmpod():
 
 
 def get_password(host, username):
-    if get_password._saved[host, username]:
+    try:
         return get_password._saved[host, username]
-    else:
+    except KeyError:
         password = getpass.getpass(prompt='Enter password for {}@{}: '
                                           .format(username, host))
         get_password._saved[host, username] = password
@@ -151,38 +152,27 @@ def make_clone_name(original_name):
 @contextlib.contextmanager
 def pysphere_connection(host, username):
     password = get_password(host, username)
-    if not DRY_RUN:
-        server = VIServer()
-        server.connect(host,
-                       username,
-                       password)
-    else:
-        server = DryRunDummy()
+    server = pysphere.VIServer()
+    server.connect(host,
+                   username,
+                   password)
     logger.debug('pysphere_connection: connected to vSphere')
     yield server
-    if not DRY_RUN:
-        server.disconnect()
+    server.disconnect()
     logger.debug('pysphere_connection: disconnected from vSphere')
 
 
 @contextlib.contextmanager
 def vsphere_connection(host, username, port):
     password = get_password(host, username)
-    if not DRY_RUN:
-        try:
-            service_instance = pyVim.connect.SmartConnect(
-                    host=host,
-                    user=username,
-                    pwd=password,
-                    port=int(port))
-        except requests.exceptions.ConnectionError as e:
-            raise ConnectionError('Failed to connect to vSphere', e)
-    else:
-        service_instance = DryRunDummy()
+    service_instance = pyVim.connect.SmartConnect(
+            host=host,
+            user=username,
+            pwd=password,
+            port=int(port))
     logger.debug('vsphere_connection: connected to vSphere')
     yield service_instance
-    if not DRY_RUN:
-        pyVim.connect.Disconnect(service_instance)
+    pyVim.connect.Disconnect(service_instance)
     logger.debug('vsphere_connection: disconnected from vSphere')
 
 
@@ -193,30 +183,72 @@ def get_command(args):
 
 
 def argvs_from_file(file):
-    for n, line in enumerate(file):
-        # Allow only the first line to start with "ALL"
-        if n == 0 and line.startswith('ALL'):
-
+    global_
+    for line in file:
+        if line.startswith('ARGS'):
+            global_args = shlex.split(line)
         yield shlex.split(line)
 
+
+def strip_default_values(s):
+    """Returns s with docopt-style default values stripped out"""
+    return re.sub(r'\[default:.*\]', '', s)
+
+
+def generate_args(arguments):
+    command = next(arg for arg in arguments if arguments[arg] and
+                                               not arg.startswith('-'))
+    if command == 'batch':
+        persistent_args = parse_args(argv=['batch'])
+        for line in sys.stdin:
+            words = shlex.split(line)
+            if words[0] == 'ARGS':
+                # Docopt needs a valid command, not 'ARGS'
+                words[0] = 'batch'
+                persistent_args = parse_args(argv=words)
+            else:
+                # Replace persistent args with explicitly-specified args from
+                # the current line
+                line_args = persistent_args.copy()
+                for option, value in parse_args(argv=words, use_defaults=False):
+                    if value:
+                        line_args[option] = value
+                yield words[0], line_args
+    else:
+        yield command, arguments
+
+
+def parse_args(argv=None, use_defaults=True):
+    """Read arguments with docopt but ignore default values in the usage.
+
+    This is useful when we want to merge one set of arguments into another,
+    and we don't want the default values in the second set to overwrite
+    explicitly-set values in the first.
+    """
+    usage = __doc__ if use_defaults else strip_default_values(__doc__)
+    return docopt.docopt(usage, argv=argv)
+
+
 if __name__ == '__main__':
+    global DRY_RUN
     # Make logging prettier
-    formatter = logging.Formatter(fmt='[%(name)s:%(levelname)s] %(message)s')
+    formatter = logging.Formatter(fmt='%(name)s: %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     del formatter, handler
 
-    # Do the things
-    arguments = docopt.docopt(__doc__)
-    argvs = argvs_from_file(sys.stdin) if arguments['batch'] else [sys.argv[1:]]
+    # Read initial invocation arguments
+    arguments = parse_args()
     DRY_RUN = arguments['--dry-run']
     if arguments['--verbose']:
         logger.setLevel(logging.DEBUG)
-    for argv in argvs:
-        args = arguments.copy()
-        args.update(docopt.docopt(__doc__, argv=argv))
-        command = get_command(args)
-        logger.debug('command is {!r}'.format(command))
-        command_func = globals()['action_{}'.format(command)]
-        sys.exit(command_func(args))
+
+    # Do the things
+    for command, args in generate_args(arguments):
+        command_func = globals()['action_%s' % command]
+        try:
+            command_func(args)
+        except MissingArgumentsError as e:
+            logger.error(e)
+            sys.exit(1)
